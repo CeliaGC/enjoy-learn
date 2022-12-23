2b48e32f5459bbfdbfd0b115a24f389fc8be9f3c
CeliaGC <you@example.comgarciacastillacelia@gmail.com>
2022-12-23 01:25:45 +0100

    add-already-works

--- play.js
+++ play.js
@@ -183,36 +183,64 @@ for (let i= 0; i < coderList.length; i++) {
     divElement.appendChild(imgElement);
 
 }
-   
+
+let newOne = document.querySelector('#buttons') 
 
 function add() {
-        let martianName = "PEPPA PIG";
-        coderList.push({name: martianName, img: 'Archivos\marcianitas\Marciano3.png', id:coderList.length + 1});
-        var divElement = document.createElement("div");
-        var pElement = document.createElement("p");
-        pElement.innerText = martianName;
-        divElement.id = "newPlayer";
-        addMartian.appendChild(divElement);
-        divElement.appendChild(pElement);
+    
+    newOne.innerHTML += `<div id="form"><input type="text" id="name" name="user_name"> 
+    <a href="#" onClick="create()" class="myButton">CREATE</a></div>`;
+   
+
 }
-
-
-
+function create() {
+    let reset = document.getElementById('form');
+    let newMartian = document.querySelector('input').value;
+    coderList.push({name: newMartian, img: 'Archivos\marcianitas\Marciano3.png', id:coderList.length});
+    var divElement = document.createElement("div");
+    var pElement = document.createElement("p");
+    pElement.innerText = newMartian;
+    divElement.id = "newPlayer";
+    addMartian.appendChild(divElement);
+    divElement.appendChild(pElement);
+    reset.innerHTML= "";
+}
    
 function winner() {
 
     let nameList = coderList.map(player => player.name);
     let grab = Math.floor(Math.random() * nameList.length - 1)+1;
     coderList.forEach(element => { if(element.id == grab){
-    addMartian.hidden;
     addMartian.innerHTML = `<div id="ganador"><h1>${element.name},</h1><br><h2>¡La garra te ha elegido!<br>!OOOOOOOOOOOOOOOOOOOH!</div>`;
-    
+
     }});
    
 }
 
-const botonGo = document.querySelector("#btPlay")
-botonGo.addEventListener("click", winner)
+function reset() {
+    addMartian.innerHTML = "";
+    printMartians();   
+}
+
+function printMartians() {
+    for (let i= 0; i < coderList.length; i++) {
+        var divElement = document.createElement("div");
+        var imgElement = document.createElement("img");
+        var pElement = document.createElement("p");
+    
+        divElement.id = "divItem" + (i + 1);
+        divElement.className = "player";
+        imgElement.id = "imgItem" + (i + 1);
+        pElement.id = "pItem" + (i + 1);
+        pElement.innerText = coderList[i].name;
+        imgElement.src= coderList[i].img;
+    
+        addMartian.appendChild(divElement);
+        divElement.appendChild(pElement);
+        divElement.appendChild(imgElement);
+    
+    }
+}
 
 
 
